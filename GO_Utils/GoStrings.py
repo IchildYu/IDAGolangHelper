79a96c5d6508457c8a2a5226fd8ea5ba64213935
IchildYu <1641727377@qq.com>
2023-06-26 21:55:50 +0800

    Update GoStrings.py

--- GO_Utils/GoStrings.py
+++ GO_Utils/GoStrings.py
@@ -1,76 +1,64 @@
-import idc
+
+import ida_bytes
 import idaapi
 import idautils
-import ida_bytes
+import idc
+from . import Utils
 
-#Simple function to fix strings in current function
-#Mostly based on "Tim 'diff' Strazzere" ideas
+ptr = Utils.get_bitness()
+def is_string(addr, length):
+    val = ptr.ptr(addr)
+    if idc.is_loaded(val): return False
+    if val < 0x100 and val % 8 == 0: return False
+    if not idc.is_loaded(addr): return False
+    if length <= 0 or not idc.is_loaded(addr + length - 1): return False
+    try:
+        b = ida_bytes.get_bytes(addr, length)
+        if 0 in b: return False
+        b.decode()
+        return True
+    except:
+        return False
 
-def parseOp(operand):
-    if operand.type == idaapi.o_displ or operand.type == idaapi.o_phrase:
-        if operand.reg == 4: #esp/rsp
-            return True, operand.addr
-    return False, 0
+def detect_string():
+    str_table = []
+    for seg_start in idautils.Segments():
+        perm = idc.get_segm_attr(seg_start, idc.SEGATTR_PERM)
+        if perm & 1: continue # x
+        if not (perm & 4): continue # r
+        seg_end = idc.get_segm_end(seg_start)
+        if seg_start & (ptr.size - 1):
+            seg_start = seg_start + (ptr.size - 1) & ~(ptr.size - 1)
+        for i in range(seg_start, seg_end - ptr.size, ptr.size):
+            length = ptr.ptr(i + ptr.size)
+            # check length
+            if length < 1 or length >= 0x800: continue
+            addr = ptr.ptr(i)
+            if length and addr not in str_table and is_string(addr, length):
+                str_table.append(addr)
+                s = ida_bytes.get_bytes(addr, length).decode()
+                print('detected string at 0x%x (0x%x, %d): %s' % (i, addr, length, s))
+                ida_bytes.del_items(addr)
+                ida_bytes.create_strlit(addr, length, -1)
 
-def convert_operand(operand, size, num, pos):
-    if operand.type == idaapi.o_displ or operand.type == idaapi.o_phrase:
-        if operand.reg == 4: #esp/rsp
-            if operand.addr < size:
-                idc.OpHex(pos, num)
-            
-#Well for now I don't know how determine ptr size ;[
-def is_this_a_real_string(next_pos, instr, size_data):
-    #Common scenario is when we mov offset into register
-    #and after that either fill local string variable or push string as argument
-    #so we just check that we use esp 
-    next_instr = idautils.DecodeInstruction(next_pos)
-    if next_instr.get_canon_mnem() == "mov":
-        is_stack_used, ptr_addr = parseOp(next_instr.Op1)
-        if is_stack_used is True:
-            #Check if we really place our offset
-            if next_instr.Op2.type == idaapi.o_reg and next_instr.Op2.reg == instr.Op1.reg: 
-                next_pos += next_instr.size
-                next_instr = idautils.DecodeInstruction(next_pos)
-                if next_instr.get_canon_mnem() == "mov":
-                    is_stack_used, size_addr = parseOp(next_instr.Op1)
-                    #if we filling string or at least smthng looking very similar
-                    if is_stack_used is True and (size_addr - ptr_addr == size_data):
-                        #for now explicitly set 0x1000 as max string size
-                        if next_instr.Op2.type == idaapi.o_imm and next_instr.Op2.value < 0x1000:
-                            return True, next_instr.Op2.value
-    return False, 0
+    str_table.sort()
+    for i in range(len(str_table) - 1):
+        addr = str_table[i]
+        end_addr = str_table[i + 1]
+        addr += idc.get_item_size(addr)
+        while addr < end_addr:
+            if idc.get_item_size(addr) != 1:
+                ida_bytes.del_items(addr)
+            if not next(idautils.XrefsTo(addr), None): break
+            length = 1
+            while idc.is_loaded(addr + length) and\
+                idc.get_item_size(addr + length) == 1 and\
+                not next(idautils.XrefsTo(addr + length), None):
+                length += 1
+            if not is_string(addr, length): break
+            s = ida_bytes.get_bytes(addr, length).decode()
+            print('extra string detected at 0x%x (%d): %s' % (addr, length, s))
+            ida_bytes.del_items(addr)
+            ida_bytes.create_strlit(addr, length, -1)
+            addr += length
 
-def make_string(addr, siz):
-    print("Creating string at %x %d size" % (addr, siz))
-    ida_bytes.del_items(addr, siz)
-    ida_bytes.create_strlit(addr, siz, -1)
-
-def get_bitness_bytes(addr):
-    if idc.get_segm_attr(addr, idc.SEGATTR_BITNESS) == 2:
-        return 8
-    return 4
-
-def stringify():
-    ea = idc.here()
-    size_data = get_bitness_bytes(ea)
-    f = idaapi.get_func(ea)
-    frsize = idc.get_func_attr(ea, idc.FUNCATTR_FRSIZE)
-    position = f.start_ea
-    size = 0
-    while position < f.end_ea:
-        instr = idautils.DecodeInstruction(position)
-        if instr is None:
-            print("%x: Not and instruction found" % position)
-            break
-        mnem = instr.get_canon_mnem()
-        if mnem == "mov":
-            if instr.Op2.type == idaapi.o_imm and instr.Op1.type == idaapi.o_reg: #this may be string load
-                is_string, size_s = is_this_a_real_string(position + instr.size, instr, size_data)
-                if is_string is True:
-                    make_string(instr.Op2.value, size_s)
-        elif mnem == "lea":
-            if instr.Op2.type == idaapi.o_mem and instr.Op1.type == idaapi.o_reg:
-                is_string, size_s = is_this_a_real_string(position + instr.size, instr, size_data)
-                if is_string is True:
-                    make_string(instr.Op2.addr, size_s)
-        position += instr.size
